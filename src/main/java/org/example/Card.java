5b31b1c35d110ea592237a96a7ead20ff2c89fab
WallochMatt <matthewrwalloch@gmail.com>
2023-02-24 12:30:43 -0600

    feat: Added gitignore + Created Player class + Added Guava dependency + Started method to create a deck

---
+++ src/main/java/org/example/Card.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Card {
+    public String suit;
+    public String value;
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,50 @@
 package org.example;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimap;
+
+import static java.lang.String.format;
+
 public class Main {
     //create
     static void runGame()
     {
         int round = 0;
         int numberOfRounds = 3;
-        int playerCount = 3;
+//        int[] playerCount = countsPlayers();
 
         while (round < numberOfRounds)
         {
             round += 1;
             System.out.println("One iteration");
         }
-    }
+    };
+
+//    static int[] countsPlayers()
+//    {
+//        int amountOfPlayers = 3;
+//        int[] players = [];
+//
+//    };
+
+
 
 
 
     public static void main(String[] args) {
         //call
-        runGame();
+//        runGame();
+//        Card card_1 = new Card("Spades", "Ace");
+//        System.out.println(card_1.value  + " of " + card_1.suit);
+        ListMultimap<String, String> deck = ArrayListMultimap.create();
 
+        for (int i = 0; i < 10; i++){
+            String newValue = String.format("%s", i + 1);
+            deck.put("Spades", newValue);
+        }
+
+        System.out.println(deck);
     }
 
 }
